a7e90e8fe347cb56ed2f2fbb08bec180daaa387e
Naveen Kumar P <naveenkumar11202006@gmail.com>
2026-04-26 14:32:43 +0530

    ju

--- src/App.tsx
+++ src/App.tsx
@@ -21,10 +21,10 @@ function App() {
           : 'bg-transparent'
         }`}>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
+          <div className="flex items-center justify-between h-24">
             {/* Logo */}
             <div className="flex items-center space-x-2">
-              <img src="/logo.png" alt="DenveX Logo" className="h-10 w-auto" />
+              <img src="/logo.png" alt="DenveX Logo" className="h-16 w-auto" />
             </div>
 
             {/* Navigation Links */}
